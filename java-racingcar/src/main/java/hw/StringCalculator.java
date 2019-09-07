75d2a55d375e7ce06df6c7f5945aefb15f279937
isooo <midnightmarching@gmail.com>
2019-09-07 13:14:21 +0900

    #2 PR : 리펙토링 - Operation을 enum으로 수정 - Calculator 내에서 계산 로직 수행 (기존 Operation에 존재했던 기능을 Calculator로 이동)

--- java-racingcar/src/main/java/hw/StringCalculator.java
+++ java-racingcar/src/main/java/hw/StringCalculator.java
@@ -18,49 +18,52 @@ public class StringCalculator {
 
 class Calculator {
     private static final String WHITE_SPACE = " ";
+
     public int execute(String input) {
         if (OperationUtils.isEmpty(input)) {
             throw new IllegalArgumentException();
         }
         final String[] arr = input.split(WHITE_SPACE);
-        String a = arr[0];
-        final Operation operation = new Operation();
+        int a = OperationUtils.parseInt(arr[0]);
         for (int i = 1; i < arr.length; i += 2) {
             String op = arr[i];
-            String b = arr[i + 1];
-            a = operation.execute(a, op, b);
+            int b = OperationUtils.parseInt(arr[i + 1]);
+            a = execute(a, op, b);
         }
-        return Integer.parseInt(a);
-    }
-}
-
-class Operation {
-    private static final String ADD = "+";
-    private static final String SUBTRACT = "-";
-    private static final String MULTIPLY = "*";
-    private static final String DIVIDE = "/";
-
-    public String execute(String a, String op, String b) {
-        final int x = OperationUtils.parseInt(a);
-        final int y = OperationUtils.parseInt(b);
-        final int result = execute(x, op, y);
-        return String.valueOf(result);
+        return a;
     }
 
     private int execute(int a, String op, int b) {
-        if (ADD.equals(op)) {
+        if (Operation.ADD.isSameSymbol(op)) {
             return a + b;
         }
-        if (SUBTRACT.equals(op)) {
+        if (Operation.SUBTRACT.isSameSymbol(op)) {
             return a - b;
         }
-        if (MULTIPLY.equals(op)) {
+        if (Operation.MULTIPLY.isSameSymbol(op)) {
             return a * b;
         }
-        if (DIVIDE.equals(op)) {
+        if (Operation.DIVIDE.isSameSymbol(op)) {
             return a / b;
         }
         throw new IllegalArgumentException("유효하지 않은 연산자입니다.");
+    }
+}
+
+enum Operation {
+    ADD("+"),
+    SUBTRACT("-"),
+    MULTIPLY("*"),
+    DIVIDE("/");
+
+    private final String symbol;
+
+    Operation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public boolean isSameSymbol(String op) {
+        return this.symbol.equals(op);
     }
 }
 
