585d2ef41a97d1d6fed0a4723756ffacfa9285a5
isooo <midnightmarching@gmail.com>
2019-10-10 20:36:15 +0900

    refactoring(racingcar) : MovingPolicy에서 magic number 제거

--- java-racingcar/src/main/java/racingcar/domain/RandomMovingPolicy.java
+++ java-racingcar/src/main/java/racingcar/domain/RandomMovingPolicy.java
@@ -3,8 +3,11 @@ package racingcar.domain;
 import java.util.*;
 
 public class RandomMovingPolicy implements MovingPolicy {
+    private static final int RANDOM_BOUND = 10;
+    private static final int NUMBER_CONDITION_TO_MOVE = 4;
+
     @Override
     public boolean isPossible() {
-        return new Random().nextInt(9) >= 4;
+        return new Random().nextInt(RANDOM_BOUND) >= NUMBER_CONDITION_TO_MOVE;
     }
 }
