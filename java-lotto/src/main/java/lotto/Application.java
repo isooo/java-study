d08ec8af1729834b6da4c82d4b1dd0fc7dc78263
isooo <midnightmarching@gmail.com>
2019-10-19 22:18:38 +0900

    feat(lotto) : 자동 로또 : Controller 및 InputView 생성

--- java-lotto/src/main/java/lotto/Application.java
+++ java-lotto/src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import lotto.ui.LottoController;
+
 public class Application {
     public static void main(String[] args) {
-        
+        LottoController.start();
     }
 }
---
+++ java-lotto/src/main/java/lotto/ui/LottoController.java
@@ -0,0 +1,6 @@
+package lotto.ui;
+
+public class LottoController {
+    public static void start() {
+    }
+}
