afa1400c0560e354251b9ab39d35686d9443fbeb
isooo <midnightmarching@gmail.com>
2019-10-26 11:07:31 +0900

    feat(lotto) : 자동 로또 : Application

--- java-lotto/src/main/java/lotto/Application.java
+++ java-lotto/src/main/java/lotto/Application.java
@@ -1,9 +1,10 @@
 package lotto;
 
+import lotto.application.LottoService;
 import lotto.ui.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController.run();
+        LottoController.run(new LottoService());
     }
 }
