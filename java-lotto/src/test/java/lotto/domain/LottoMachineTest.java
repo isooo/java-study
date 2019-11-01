c4b8331fae3b58b78ff67de59c153faa04582393
isooo <midnightmarching@gmail.com>
2019-11-01 23:58:28 +0900

    refactoring(lotto) : 자동 로또 : 테스트 케이스 수정 - 랜덤한 번호에 대한 테스트를 (로또 2개를 생성. 각 요소가 겹치는지 비교) 로 진행하였으나, 동일한 숫자로 이루어질 수도 있으므로 테스트 시나리오 변경 - 1 ~ 45 사이의 6개의 숫자가 생성되었는지 테스트 ==> 경계값 테스트도 동시에 진행하도록 함. assert를 한 메서드 안에서 2번 썼으나, 이정도의 테스트는 함께 이루어져도 된다고 판단함.

--- java-lotto/src/test/java/lotto/domain/LottoMachineTest.java
+++ java-lotto/src/test/java/lotto/domain/LottoMachineTest.java
@@ -4,23 +4,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LottoMachineTest {
-    @DisplayName("랜덤한 6자리 번호 생성하기")
+    @DisplayName("1 ~ 45 사이 랜덤한 6개의 번호 생성하기")
     @Test
     void getRandom6Numbers() {
         // given
-        final List<Integer> numbers1 = LottoMachine.getNumbers();
-        final List<Integer> numbers2 = LottoMachine.getNumbers();
+        final List<Integer> numbers = LottoMachine.getNumbers();
 
         // when
-        final boolean isSame = IntStream.rangeClosed(1, 6)
-                .allMatch(i -> numbers1.get(i) == numbers2.get(i));
+        final boolean result = numbers.stream()
+                .allMatch(number -> 1 <= number && number <= 45);
 
         // then
-        assertFalse(isSame);
+        assertTrue(result);
+        assertThat(numbers.size()).isSameAs(6);
     }
 }
