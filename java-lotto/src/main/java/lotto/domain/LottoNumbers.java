4e6c842bdc8960a075af81a8703f0a18a987ade4
isooo <midnightmarching@gmail.com>
2019-11-24 23:43:41 +0900

    refactoring(lotto) : 자동 로또 : 중복 번호를 validate 하는 메서드 제거 (Set 을 사용하기 때문에 불필요함)

--- java-lotto/src/main/java/lotto/domain/LottoNumbers.java
+++ java-lotto/src/main/java/lotto/domain/LottoNumbers.java
@@ -6,12 +6,10 @@ import java.util.stream.Collectors;
 public class LottoNumbers {
     static final int TOTAL_LOTTO_COUNT = 6;
 
-    // TODO 5: 요소의 중복을 방지하기 위해 컬렉션을 Set으로 변경
     private final Set<LottoNumber> values;
 
     public LottoNumbers(final List<LottoNumber> values) {
-        validate(values);
-        this.values = new LinkedHashSet<>(values);
+        this.values = validate(new LinkedHashSet<>(values));
     }
 
     LottoNumbers(int... values) {
@@ -24,24 +22,11 @@ public class LottoNumbers {
         return new ArrayList<>(this.values);
     }
 
-    private void validate(final List<LottoNumber> values) {
-        checkDuplicate(values);
-        checkTotalCount(values);
-    }
-
-    private void checkDuplicate(final List<LottoNumber> values) {
-        final long count = values.stream()
-                .distinct()
-                .count();
-        if (TOTAL_LOTTO_COUNT != count) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void checkTotalCount(final List<LottoNumber> values) {
+    private Set<LottoNumber> validate(final Set<LottoNumber> values) {
         if (TOTAL_LOTTO_COUNT != values.size()) {
             throw new IllegalArgumentException();
         }
+        return values;
     }
 
     public long calculateCountOfWinningNumbers(final LottoNumbers winningLottoNumbers) {
